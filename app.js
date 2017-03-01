cffa9194502f2226d0c225a2c13da7c4b68292fa
miladVisionCollege <miladthecomputerguy@gmail.com>
2017-03-01 17:11:22 +1300

    init commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var Twitter = require('twitter');
+var api = require("./api.json");
+
+function printError(error){
+	console.error(error.message);
+}
+
+var client = new Twitter({
+  consumer_key: api.consumer_key,
+  consumer_secret: api.consumer_secret,
+  access_token_key: api.access_token_key,
+  access_token_secret: api.access_token_secret
+});
+function get(query){ 
+	var account = query[0];
+	var params = {screen_name: account};
+	var request = client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		console.log(response.statusCode);
+		if(response.statusCode === 200){
+			console.log(tweets[0].text);
+		}else{
+			printError({message : "there was a problem with getting latest tweet from "+account});
+		}
+	});
+
+}
+
+const query = process.argv.slice(2);
+
+get(query);
